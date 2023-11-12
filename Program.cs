336acfe587d2df8cf868d10e24cde38aac3c06c9
Randitya Hundal <67023582+RandityaHundal@users.noreply.github.com>
2023-11-11 23:34:04 -0800

    Add files via upload
    code

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// a method that counts the number of words in a sentence
+// take in string, the sentence
+// return the number of words as an integer
+
+// Prompt to input a sentence and store as a string
+Console.WriteLine("Input sentence:");
+string sentence = Console.ReadLine();
+
+// use getWordCount method that is below
+int result = getWordCount(sentence);
+
+// Return how many words the sentence has
+Console.WriteLine($"Your sentence has {result} words.");
+
+//close with key
+Console.ReadKey();
+
+// getWordCount method
+static int getWordCount(string sentence)
+{
+
+    // splits after every space to count words then puts into an array
+    string[] words = sentence.Split(' ');
+    // count the array length
+    int wordCount = words.Length;
+    // return wordCount
+    return wordCount;
+
+}
